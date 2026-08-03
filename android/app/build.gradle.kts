46d07a6b13f080b326f955dcc2b04fdf7ee853de
Pasquale-Perilli <pperilli@gritterfrancona.us>
2026-08-03 05:29:30 -0400

    Update Android compileSdk to 36
    - Required by share_plus plugin dependencies
    - AndroidX libraries require SDK 34 or later

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.github.pasqualeperilli.worder"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
